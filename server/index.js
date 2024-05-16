16baf2b79bd255889788850a719494a545d7424b
nrmnkny <normane7@icloud.com>
2024-05-16 17:21:14 +0300

    redos

--- server/index.js
+++ server/index.js
@@ -7,18 +7,24 @@ const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:3000', 'https://rhythmiccli.vercel.app'] })); 
+// Update the CORS configuration to allow requests from your frontend URL
+app.use(cors({ origin: ['http://localhost:3000', 'https://rhythmiccli.vercel.app'] }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Use routes
 app.use('/api/reviews', reviewsRouter);
-app.use('/api/auth', authRouter); 
+app.use('/api/auth', authRouter);
 
 // Handle 404
 app.use((req, res) => {
     res.status(404).send('404: Page not found');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
